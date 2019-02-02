d33c33e5854bc881a52880afdb379846f842fc49
amverni <amverni@umich.edu>
2019-02-02 14:55:25 -0500

    [nav] changed nav to expect Odom lcm, changed filter to expect imu not mag lcm

--- onboard/filter/src/__main__.py
+++ onboard/filter/src/__main__.py
@@ -2,14 +2,9 @@ import time
 import enum
 from rover_common import aiolcm
 import asyncio
-from rover_msgs import Bearing
-from rover_msgs import Odometry
-from rover_msgs import NavStatus
+from rover_msgs import IMU, GPS, NavStatus
 from rover_common.aiohelper import run_coroutines
-from .rawmessages import mag_bearing
-from .rawmessages import raw_gps
-from .rawmessages import nav_status
-from .rawmessages import clean_odom
+from .rawmessages import raw_imu, raw_gps, nav_status, clean_odom
 
 # from rover_msgs import Wheelenc
 # from rover_msgs import IMU
@@ -40,44 +35,44 @@ class NavState(enum.Enum):
 
 class FilterClass:
     def __init__(self):
-        self.gps = raw_gps()
-        self.imu_bearing = mag_bearing()
-        self.navstat = nav_status()
-        self.odomf = clean_odom()
+        self._gps = raw_gps()
+        self._imu = raw_imu()
+        self._navstat = nav_status()
+        self._odomf = clean_odom()
 
     def gps_callback(self, channel, msg):
         print('-------------- @time : ' + str(time.clock()))
         print('gps callback called')
-        m = Odometry.decode(msg)
-        self.gps.updateGPS(m)
-        self.odomf.copy_gps(self.gps)
-        print('gps track angle: ' + str(self.gps.track_theta))
+        m = GPS.decode(msg)
+        self._gps.updateGPS(m)
+        self._odomf.copy_gps(self.gps)
+        print('gps track angle: ' + str(self._gps.track_theta))
         return None
 
-    def mag_bearing_callback(self, channel, msg):
+    def imu_bearing_callback(self, channel, msg):
         print('-------------- @time : ' + str(time.clock()))
         print('imu callback called')
-        m = Bearing.decode(msg)
-        self.imu_bearing.update_mag_bearing(m)
-        print('mag bearing: ' + str(self.imu_bearing.mbearing))
+        m = IMU.decode(msg)
+        self._imu.update_imu_bearing(m)
+        print('mag bearing: ' + str(self._imu._bearing))
         return None
 
     def navstat_callback(self, channel, msg):
         print('-------------- @time : ' + str(time.clock()))
         print('navstat callback called')
         m = NavStatus.decode(msg)
-        self.navstat.update_nav_status(m)
+        self._navstat.update_nav_status(m)
         if self.turning():
             print('turning')
         else:
             print("not turning")
-        print('nav status: ' + str(self.navstat.navState))
+        print('nav status: ' + str(self._navstat.navState))
         return None
 
     def stationary(self):
         """Determine if rover is stationary."""
-        if self.navstat == NavState.Off or \
-           self.navstat == NavState.Done:
+        if self._navstat == NavState.Off or \
+           self._navstat == NavState.Done:
             return True
         return False
 
@@ -115,7 +110,7 @@ class FilterClass:
         while True:
             print('async af')
             # self.filter_bearing()
-            msg = self.odomf.create_lcm()
+            msg = self._odomf.create_lcm()
             lcm_.publish('/odometryf', msg.encode())
             await asyncio.sleep(1)
 
@@ -126,8 +121,8 @@ def main():
     lcm_ = aiolcm.AsyncLCM()
     filter_ = FilterClass()
 
-    lcm_.subscribe("/odometry", filter_.gps_callback)
-    lcm_.subscribe("/bearing", filter_.mag_bearing_callback)
+    lcm_.subscribe("/gps", filter_.gps_callback)
+    lcm_.subscribe("/imu", filter_.mag_bearing_callback)
     lcm_.subscribe("/nav_status", filter_.navstat_callback)
 
     run_coroutines(lcm_.loop(), filter_.publishOdom(lcm_))
